b300758f80dfc24e1da2a3d5b7854ffd59414a0c
Thomas Gordon <teegee@mm.st>
2012-04-24 15:22:15 -0400

    Adding more elements still

--- ps-day-punch.user.js
+++ ps-day-punch.user.js
@@ -36,25 +36,42 @@ function addControls() {
     GM_log("--> addControls()");
 
     var dpButtonContainerStyle = "display: inline; margin: 0; border: 4px solid #ff4a00; padding: 4px; padding-top: 2px;";
-        var dpButtonStyle      = "display: inline; margin: 0; padding: 0;";
         var setButtonStyle     = "display: inline; margin: 0; padding: 0;";
+        var showButtonStyle    = "display: inline; margin: 0; padding: 0;";
+        var dpButtonStyle      = "display: inline; margin: 0; padding: 0;";
+        var wpButtonStyle      = "display: inline; margin: 0; padding: 0;";
 
     var buttonContainer = document.getElementById("win0divTL_LINK_WRK_TL_ADD_PB");
 
     var dpButtonContainer = document.createElement("div");
     dpButtonContainer.setAttribute("style", dpButtonContainerStyle);
-        var dayPunchButton = document.createElement("input");
-        dayPunchButton.setAttribute("style", dpButtonStyle);
-        dayPunchButton.setAttribute("type", "button");
-        dayPunchButton.setAttribute("value", "Day Punch");
         var setPunchButton = document.createElement("input");
         setPunchButton.setAttribute("style", setButtonStyle);
         setPunchButton.setAttribute("type", "button");
+        setPunchButton.setAttribute("title", "Save currently displayed punches as typical work week");
         setPunchButton.setAttribute("value", "Set Punches");
+        var showPunchButton = document.createElement("input");
+        showPunchButton.setAttribute("style", showButtonStyle);
+        showPunchButton.setAttribute("type", "button");
+        showPunchButton.setAttribute("title", "Show saved punches");
+        showPunchButton.setAttribute("value", "Show Punches");
+        var dayPunchButton = document.createElement("input");
+        dayPunchButton.setAttribute("style", dpButtonStyle);
+        dayPunchButton.setAttribute("type", "button");
+        dayPunchButton.setAttribute("title", "Punch in a single day");
+        dayPunchButton.setAttribute("value", "Day Punch");
+        var weekPunchButton = document.createElement("input");
+        weekPunchButton.setAttribute("style", wpButtonStyle);
+        weekPunchButton.setAttribute("type", "button");
+        weekPunchButton.setAttribute("title", "Punch in an entire week");
+        weekPunchButton.setAttribute("value", "Week Punch");
+
 
     buttonContainer.appendChild(dpButtonContainer);
-        dpButtonContainer.appendChild(dayPunchButton);
         dpButtonContainer.appendChild(setPunchButton);
+        dpButtonContainer.appendChild(showPunchButton);
+        dpButtonContainer.appendChild(dayPunchButton);
+        dpButtonContainer.appendChild(weekPunchButton);
 }
 
 try {
