973c40767f40a0a927b49a8678854236b31ed3e8
Thomas Gordon <teegee@mm.st>
2012-06-22 09:08:05 -0400

    Matching dates to days of the week in various places

--- ps-day-punch.user.js
+++ ps-day-punch.user.js
@@ -166,7 +166,7 @@ function setMostRecentDate() {
 
     var tmpFirstPunchRow = document.getElementById("trTL_RPTD_PCHTIME$0_row1");
 
-    if ((typeof tmpFirstPunchRow != undefined) || (tmpFirstPunchRow != '')) {
+    if ((typeof tmpFirstPunchRow != undefined) || (tmpFirstPunchRow.value != '')) {
         GM_log('this is happening');
         var punchRowParent = tmpFirstPunchRow.parentNode;
         var someRandomWhitespaceApparently = punchRowParent.lastChild;
